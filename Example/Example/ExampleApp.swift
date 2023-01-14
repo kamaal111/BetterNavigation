131548eff127eb3b57f9cf63fccaec5f9e064674
Kamaal <kamaal.f1@gmail.com>
2023-01-14 16:22:10 +0100

    Initializing example app

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Kamaal M Farah on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
